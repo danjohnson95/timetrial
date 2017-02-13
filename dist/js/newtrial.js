d18d8e889168983cc19ee236a69dec51b5b88dd3
Dan Johnson <dan@danj.eu>
2017-02-13 22:27:32 +0000

    Added clientside validation

--- dist/js/newtrial.js
+++ dist/js/newtrial.js
@@ -2,7 +2,9 @@ const validate = require('./src/js/validate.js');
 
 const obj = {
 
-	elements: {},
+	elements: {
+		form: {}
+	},
 
 	init: function(){
 		this.getElements();
@@ -13,6 +15,13 @@ const obj = {
 		this.elements.addbtn = document.getElementById('new-trial');
 		this.elements.newform = document.getElementById('new-form');
 		this.elements.saverun = document.getElementById('save-and-run');
+
+		this.elements.form.name = this.elements.newform.querySelector('input[name=name]');
+		this.elements.form.description = this.elements.newform.querySelector('textarea[name=description]');
+		this.elements.form.html = this.elements.newform.querySelector('textarea[name=html]');
+		this.elements.form.code_1 = this.elements.newform.querySelector('textarea[name=code_1]');
+		this.elements.form.code_2 = this.elements.newform.querySelector('textarea[name=code_2]');
+
 	},
 
 	registerEventListeners: function(){
@@ -28,15 +37,38 @@ const obj = {
 	},
 
 	getValues: function(){
-		return {};
+		return {
+			name: obj.elements.form.name.value,
+			description: obj.elements.form.description.value,
+			html: obj.elements.form.html.value,
+			code_1: obj.elements.form.code_1.value,
+			code_2: obj.elements.form.code_2.value
+		};
+	},
+
+	handleErrors: function(errors){
+		console.log(errors);
+		errors.forEach(function(e){
+			obj.elements.form[e.field].classList.add('error');
+
+		});
+	},
+
+	resetErrors: function(){
+		var elm;
+		Object.keys(obj.elements.form).forEach(function(name){
+			elm = obj.elements.form[name];
+			if(elm.classList.contains('error')) elm.classList.remove('error');
+		});
 	},
 
 	saverun: function(){
-		// TODO: Client side checks here.
+		obj.resetErrors();
 		var values = obj.getValues();
-		validate.trial(values, function(passed, obj){
+		validate.trial(values, function(passed, errors){
 			console.log(passed);
-			if(!passed) return obj.handleErrors(obj);
+			if(!passed) return obj.handleErrors(errors);
+			console.log('passed?');
 		});
 	}
 
