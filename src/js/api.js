0314e63820ab3e95e01b6b76a3954960f16f1656
Dan Johnson <dan@danj.eu>
2017-02-13 21:23:10 +0000

    Added documentation to API functions

--- src/js/api.js
+++ src/js/api.js
@@ -2,10 +2,63 @@ const store = require('./store.js');
 
 var obj = {
 
+	/**
+	 * Gets all the trials created by the user
+	 * @param callback
+	 */
 	getMyTrials: function(callback){
 		store.trials.find({}, function(err, trials){
 			callback(trials);
 		});
+	},
+
+	/**
+	 * Returns API errors back to the user
+	 * @param obj The reason why the API call failed
+	 * @param callback
+	 */
+	error: function(obj, callback){
+		return callback(obj, null);
+	},
+
+	/**
+	 * Ensures data meets requirements
+	 * @param obj Data about to be put in storage
+	 * @param callback
+	 */
+	validate: function(data, callback){
+
+	},
+
+	/**
+	 * Creates a new resource in storage
+	 * @param obj Data to be stored
+	 * @param callback
+	 */
+	new: function(data, callback){
+		this.validate(data, function(valid){
+			if(!valid) this.error(valid, callback);
+		});
+	},
+
+	/**
+	 * Updates an existing resource in storage
+	 * @param obj Resource to be updated, with the new data
+	 * @param callback
+	 */
+	update: function(data, callback){
+		this.validate(data, function(valid){
+			if(!valid) this.error(valid, callback);
+		});
+	},
+
+	/**
+	 * Deletes an existing resource in storage
+	 * @param obj Resource to be deleted
+	 * @param callback
+	 */
+	delete: function(data, callback){
+
 	}
 
 };
