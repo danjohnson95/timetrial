84b03b3fa683d2a1948f1b72e383e729fb0812c0
Dan Johnson <dan@danj.eu>
2017-02-13 21:27:50 +0000

    Created validation function in API

--- src/js/api.js
+++ src/js/api.js
@@ -27,7 +27,16 @@ var obj = {
 	 * @param callback
 	 */
 	validate: function(data, callback){
+		var errors = [];
+		if(!"name" in data || data.name == "")
+			errors.push({"field": "name", "msg": "Name cannot be empty"});
+		if(!"code_1" in data || data.code_1 == "")
+			errors.push({"field": "code_1", "msg": "First trial cannot be empty"});
+		if(!"code_2" in data || data.code_2 == "")
+			errors.push({"field": "code_2", "msg": "Second trial cannot be empty"});
 
+		if(errors.length) return callback(false, errors);
+		return callback(true, null);
 	},
 
 	/**
@@ -36,8 +45,8 @@ var obj = {
 	 * @param callback
 	 */
 	new: function(data, callback){
-		this.validate(data, function(valid){
-			if(!valid) this.error(valid, callback);
+		this.validate(data, function(passed, obj){
+			if(!passed) this.error(obj, callback);
 		});
 	},
 
@@ -47,8 +56,8 @@ var obj = {
 	 * @param callback
 	 */
 	update: function(data, callback){
-		this.validate(data, function(valid){
-			if(!valid) this.error(valid, callback);
+		this.validate(data, function(passed, obj){
+			if(!passed) this.error(obj, callback);
 		});
 	},
 
